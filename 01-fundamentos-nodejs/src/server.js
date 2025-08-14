adae34dc1ab23aa0e01d26777382f38897694287
Leonardo Carvalho <leu1.g.carvalho@gmail.com>
2025-08-14 14:46:14 -0300

    estudos node - buffer | stream

--- 01-fundamentos-nodejs/src/server.js
+++ 01-fundamentos-nodejs/src/server.js
@@ -1,4 +1,5 @@
 import http from "node:http";
+import { json } from "./middlewares/json.js";
 
 // metodo e url
 // basicamente a rota que se forma
@@ -17,20 +18,22 @@ import http from "node:http";
 const users = [];
 
 // req == request || res == response
-const server = http.createServer((req, res) => {
+const server = http.createServer(async (req, res) => {
   const { method, url } = req;
 
+  await json(req, res);
+
   if (method === "GET" && url === "/users") {
-    return res
-      .setHeader("Content-type", "application/json")
-      .end(JSON.stringify(users));
+    return res.end(JSON.stringify(users));
   }
 
   if (method === "POST" && url === "/users") {
+    const { name, email } = req.body;
+
     users.push({
-      id: 1,
-      name: "Leo Carva",
-      email: "leocarva@example.com",
+      id: users.length,
+      name,
+      email,
     });
 
     return res.writeHead(201).end();
--- 01-fundamentos-nodejs/streams/fake-upload-to-http-stream.js
+++ 01-fundamentos-nodejs/streams/fake-upload-to-http-stream.js
@@ -7,7 +7,7 @@ class OneToHundredStream extends Readable {
     const i = this.index++;
 
     setTimeout(() => {
-      if (i > 100) {
+      if (i > 5) {
         this.push(null);
       } else {
         const buf = Buffer.from(String(i));
@@ -22,5 +22,11 @@ class OneToHundredStream extends Readable {
 fetch("http://localhost:3334", {
   method: "POST",
   body: new OneToHundredStream(),
-  duplex: 'half',
-});
+  duplex: "half",
+})
+  .then((response) => {
+    return response.text();
+  })
+  .then((data) => {
+    console.log(data);
+  });
